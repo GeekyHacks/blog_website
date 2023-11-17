0939481d5389ff82c5ee609258e492f8b7b18b91
Abdullah <geekyhacks22@gmail.com>
2023-11-18 00:58:45 +0300

    Add Models validations and spec files

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,14 @@ class Post < ApplicationRecord
   has_many :comments
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
+  # validations
+  validates :title, presence: true, format: { without: /\d/, message: 'should not contain numbers' },
+                    length: { minimum: 3, maximum: 50 }, uniqueness: true
+  validates :text, presence: true, length: { minimum: 20, maximum: 250 }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,13 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
-  # validaions
-  validates :name, presence: true, length: { minimum: 3 }
+
+  # validations
+  validates :name, presence: true, format: { without: /\d/, message: 'should not contain numbers' },
+                   length: { minimum: 3, maximum: 50 }, uniqueness: true
+  validates :photo, presence: true, uniqueness: true
+  validates :bio, presence: true, length: { minimum: 20, maximum: 145 }
+  validates :postcounter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
