9dfe3999dad1bc7ad3de6389dcacfde7786edd08
Abdullah <geekyhacks22@gmail.com>
2023-11-17 08:09:35 +0300

    Add required changes

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,8 @@ class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  after_save :update_comments_counter
+
   def update_likes_counter
     post.update(likes_count: post.likes.count)
   end
