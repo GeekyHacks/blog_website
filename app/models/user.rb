cbb832c49dc622505537795c9b248c61af63eb0f
Abdullah <geekyhacks22@gmail.com>
2023-11-18 00:18:23 +0300

    Add spec configuration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+  # validaions
+  validates :name, presence: true, length: { minimum: 3 }
 
   def recent_posts
     posts.order(created_at: :desc).limit(3)
